822f95b8d21801345e4e2bb0b86ca4cb7c7d7747
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2023-10-26 15:28:41 -0300

    Add `magrittr` as a dependency.

--- R/hello.R
+++ R/hello.R
@@ -22,10 +22,37 @@ setup <- function() {
   return(invisible(NULL))
 }
 
-
-foo <- function() {
-  DT <- JuliaConnectoR::juliaImport("DataFrames")
-  x <- DT$DataFrame() |> as.data.frame()
-  return(x)
-}
-foo()
+# foo <- function(
+#     Y,
+#     p = 1L,
+#     z0 = 1.0,
+#     q0 = 1.0,
+#     v0 = ncol(Y) + 1L,
+#     S0 = diag(ncol(Y)),
+#     warmup = 2000L,
+#     iter = 4000L,
+#     thin = 1L,
+#     hmax = 1L,
+#     ...
+# ) {
+#   BV <- JuliaConnectoR::juliaImport("BNPVAR")
+#   out_jl <-
+#     BV$fit(
+#       Y,
+#       p = p,
+#       z0 = z0,
+#       q0 = q0,
+#       v0 = v0,
+#       S0 = S0,
+#       warmup = warmup,
+#       iter = iter,
+#       thin = thin,
+#       hmax = hmax
+#     )
+#   out <-
+#     list(
+#       gamma = out_jl$gamma |> dplyr::as_tibble(),
+#       irf = out_jl$irf |> dplyr::as_tibble()
+#     )
+#   return(out)
+# }
